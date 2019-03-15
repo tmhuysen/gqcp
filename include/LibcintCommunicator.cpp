7f49b4294775d7d0a3e5eee09e45bd38b6d8eb7a
Laurent Lemmens <laurent_lemmens@hotmail.com>
2019-03-15 12:41:55 +0100

    start

--- include/LibcintCommunicator.cpp
+++ include/LibcintCommunicator.cpp
@@ -51,13 +51,6 @@ void LibcintCommunicator::test() const {
     auto atoms = basisset.atoms();
 
 
-//    std::cout << "Atoms" << std::endl;
-//    for (const auto& atom : atoms) {
-//        std::cout << atom << std::endl;
-//    }
-//    std::cout << std::endl;
-
-
     int natm = static_cast<int>(atoms.size());
     int nbf = 2;  // number of basis functions
 
@@ -98,105 +91,107 @@ void LibcintCommunicator::test() const {
      */
 
 
-//    int atom_index = 0;  // index of the atom the shell is centered on
-//    auto previous_atom = basisset[0].get_atom();
-//
-//
-//    for (size_t n = 0; n < basisset.numberOfShells(); n++) {
-//
-//        auto current_shell = basisset[n];
-//        auto contractions = current_shell.get_contractions();
-//
-//
-//
-//        // If there's a new atom, increment the index
-//        auto current_atom = current_shell.get_atom();
-//        if (current_atom != previous_atom) {
-//            atom_index++;
-//            previous_atom = current_atom;
-//        }
-//        libcint_bas[ATOM_OF + BAS_SLOTS * n] = atom_index;
-//
-//
-//
-//        for (size_t m = 0; m < contractions.size(); m++) {
-//            auto current_contraction = contractions[m];
-//
-//            libcint_bas[ANG_OF   + BAS_SLOTS * n] = static_cast<int>(current_contraction.l);  // angular momentum
-//            libcint_bas[NPRIM_OF + BAS_SLOTS * n] = static_cast<int>(current_contraction.length());  // number of primitives
-//            libcint_bas[NCTR_OF  + BAS_SLOTS * n] = static_cast<int>(current_shell.numberOfContractions());  // number of contractions  // FIXME this can't be right
-//
-//            libcint_bas[PTR_EXP  + BAS_SLOTS * n] = offset;  // pointer to the exponents of the shell inside the libcint environment
-//            for (size_t e = 0; e < current_contraction.length(); e++, offset++) {  // also increment offset
-//                libcint_env[offset + e] = current_shell.get_exponents()[e];
-//            }
-//
-//
-//            libcint_bas[PTR_COEFF + BAS_SLOTS * n] = offset;  // pointer to the contraction coefficients inside the libcint environment
-//            // input normalized coeff.
-//            for (size_t c = 0; c < current_contraction.length(); c++, offset++) {  // also increment offset
-//
-//
-//                libcint_env[offset + c] = current_contraction.coefficients[c] * CINTgto_norm(libcint_bas[ANG_OF+BAS_SLOTS*n], libcint_env[libcint_bas[PTR_EXP+BAS_SLOTS*n]+c]);
-//            }
-//
-//
-//        }
-//
-//    }
-
-    int n = 0;
-    /* basis #0 */
-    libcint_bas[ATOM_OF  + BAS_SLOTS * n]  = 0;
-    libcint_bas[ANG_OF   + BAS_SLOTS * n]  = 0;
-    libcint_bas[NPRIM_OF + BAS_SLOTS * n]  = 3;
-    libcint_bas[NCTR_OF  + BAS_SLOTS * n]  = 1;
-    libcint_bas[PTR_EXP  + BAS_SLOTS * n]  = offset;
-    libcint_env[offset + 0] = 3.42525091;
-    libcint_env[offset + 1] = 0.623913730;
-    libcint_env[offset + 2] = 0.168855400;
-    offset += 3;
-    libcint_bas[PTR_COEFF+ BAS_SLOTS * n] = offset;
-    libcint_env[offset + 0] = 0.154328970 * CINTgto_norm(libcint_bas[ANG_OF+BAS_SLOTS*n], libcint_env[libcint_bas[PTR_EXP+BAS_SLOTS*n]+0]);
-    libcint_env[offset + 1] = 0.535328140 * CINTgto_norm(libcint_bas[ANG_OF+BAS_SLOTS*n], libcint_env[libcint_bas[PTR_EXP+BAS_SLOTS*n]+1]);
-    libcint_env[offset + 2] = 0.444634540 * CINTgto_norm(libcint_bas[ANG_OF+BAS_SLOTS*n], libcint_env[libcint_bas[PTR_EXP+BAS_SLOTS*n]+2]);
-    offset += 3;
-    n++;
-
-    /* basis #1 */
-    libcint_bas[ATOM_OF  + BAS_SLOTS * n] = 1;
-    libcint_bas[ANG_OF   + BAS_SLOTS * n] = libcint_bas[ANG_OF   + BAS_SLOTS * 0];
-    libcint_bas[NPRIM_OF + BAS_SLOTS * n] = libcint_bas[NPRIM_OF + BAS_SLOTS * 0];
-    libcint_bas[NCTR_OF  + BAS_SLOTS * n] = libcint_bas[NCTR_OF  + BAS_SLOTS * 0];
-    libcint_bas[PTR_EXP  + BAS_SLOTS * n] = libcint_bas[PTR_EXP  + BAS_SLOTS * 0];
-    libcint_bas[PTR_COEFF+ BAS_SLOTS * n] = libcint_bas[PTR_COEFF+ BAS_SLOTS * 0];
-    n++;
+    int atom_index = 0;  // index of the atom the shell is centered on
+    auto previous_atom = basisset[0].get_atom();
 
 
-    std::cout << "Libcint output: " << std::endl;
+    for (size_t n = 0; n < basisset.numberOfShells(); n++) {
+
+        auto current_shell = basisset[n];
+        auto contractions = current_shell.get_contractions();
+
+
+
+        // If there's a new atom, increment the index
+        auto current_atom = current_shell.get_atom();
+        if (current_atom != previous_atom) {
+            atom_index++;
+            previous_atom = current_atom;
+        }
+        libcint_bas[ATOM_OF + BAS_SLOTS * n] = atom_index;
+
+
+
+        for (size_t m = 0; m < contractions.size(); m++) {
+            auto current_contraction = contractions[m];
+
+            libcint_bas[ANG_OF   + BAS_SLOTS * n] = static_cast<int>(current_contraction.l);  // angular momentum
+
+            libcint_bas[NPRIM_OF + BAS_SLOTS * n] = static_cast<int>(current_contraction.length());  // number of primitives
+
+            libcint_bas[NCTR_OF  + BAS_SLOTS * n] = static_cast<int>(current_shell.numberOfContractions());  // number of contractions  // FIXME this can't be right if >1 contraction with the same angular momentum
+
+            libcint_bas[PTR_EXP  + BAS_SLOTS * n] = offset;  // pointer to the exponents of the shell inside the libcint environment
+
+            for (size_t e = 0; e < current_contraction.length(); e++, offset++) {  // also increment offset
+                libcint_env[offset] = current_shell.get_exponents()[e];
+            }
+
+
+            libcint_bas[PTR_COEFF + BAS_SLOTS * n] = offset;  // pointer to the contraction coefficients inside the libcint environment
+
+
+            // input normalized coeff.
+            for (size_t c = 0; c < current_contraction.length(); c++, offset++) {  // also increment offset
+                libcint_env[offset] = current_contraction.coefficients[c] * CINTgto_norm(libcint_bas[ANG_OF+BAS_SLOTS*n], libcint_env[libcint_bas[PTR_EXP+BAS_SLOTS*n]+c]);
+            }
+
+
+        }
+
+    }
 
 
     // CALCULATE OVERLAP INTEGRALS
 
-    for (size_t i = 0; i < basisset.numberOfShells(); i++) {
-        for (size_t j = 0; j < basisset.numberOfShells(); j++) {
+    for (size_t sh1 = 0; sh1 < basisset.numberOfShells(); sh1++) {
+        for (size_t sh2 = 0; sh2 < basisset.numberOfShells(); sh2++) {
 
             int shls[2];
-            shls[0] = static_cast<int>(i);
-            shls[1] = static_cast<int>(j);
+            shls[0] = static_cast<int>(sh1);
+            shls[1] = static_cast<int>(sh2);
 
-            int dim_i = CINTcgto_cart(static_cast<int>(i), libcint_bas);  // what's this?
-            int dim_j = CINTcgto_cart(static_cast<int>(j), libcint_bas);
+            int nbf_sh1 = CINTcgto_cart(static_cast<int>(sh1), libcint_bas);  // number of basis functions in first shell
+            int nbf_sh2 = CINTcgto_cart(static_cast<int>(sh2), libcint_bas);  // number of basis functions in second shell
 
-            std::cout << "dim i: " << dim_i << "  dim j: " << dim_j << std::endl;
+            std::cout << "dim i: " << nbf_sh1 << "  dim j: " << nbf_sh2 << std::endl;
 
-            double* buf = (double*)malloc(sizeof(double) * dim_i * dim_j);  // buffer where the integrals are calculated to
+            double* buf = (double*)malloc(sizeof(double) * nbf_sh1 * nbf_sh2);  // buffer where the integrals are calculated to
             cint1e_ovlp_cart(buf, shls, libcint_atm, natm, libcint_bas, nbf, libcint_env);
             std::cout << buf[0] << std::endl;
 
             free(buf);
         }
     }
+
+
+    const auto nsh = static_cast<size_t>(basisset.size());  // number of shells
+    for (auto sh1 = 0; sh1 != nsh; ++sh1) {  // shell 1
+        for (auto sh2 = 0; sh2 != nsh; ++sh2) {  // shell 2
+            // Calculate integrals between the two shells
+            engine.compute(basisset[sh1], basisset[sh2]);  // this updates the pointers in calculated_integrals
+
+
+            // Place the calculated integrals into the matrix representation(s): the integrals are stored in row-major form
+            auto bf1 = shell2bf[sh1];  // (index of) first bf in sh1
+            auto bf2 = shell2bf[sh2];  // (index of) first bf in sh2
+
+            auto nbf_sh1 = basisset[sh1].size();  // number of basis functions in first shell
+            auto nbf_sh2 = basisset[sh2].size();  // number of basis functions in second shell
+
+            for (auto f1 = 0; f1 != nbf_sh1; ++f1) {  // f1: index of basis function within shell 1
+                for (auto f2 = 0; f2 != nbf_sh2; ++f2) { // f2: index of basis function within shell 2
+
+                    for (size_t i = 0; i < N; i++) {
+                        double computed_integral = calculated_integrals[i][f2 + f1 * nbf_sh2];  // integrals are packed in row-major form
+                        operator_components[i](bf1 + f1, bf2 + f2) = computed_integral;
+                    }
+
+                }
+            }  // data access loops
+
+        }
+    }  // shell loops
 
     free(libcint_atm);
     free(libcint_bas);
